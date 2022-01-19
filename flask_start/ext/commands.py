3551bd33f2f6c67a564a389ec68ee38fe5c592f6
imakecodes <imakecodes@users.noreply.github.com>
2022-01-19 03:07:10 +0000

    ✅ Ready to clone and code.

--- flask_start/ext/commands.py
+++ flask_start/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flask_start.ext.auth import create_user
+from flask_start.ext.database import db
+from flask_start.models import Product
 
 
 def create_db():
